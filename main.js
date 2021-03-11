e4e2a354ed75b2b14ea8f458133b11c72c651aed
Roy <b.roynaim@gmail.com>
2021-03-11 16:43:33 +0100

    document ready created

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.next').click(function(){
+    console.log("ok");
+  })
+})
+
+
+
+
+//
+// function() {
+//
+// }
+// var next = $('.next');
+//
+// next.click(function(){
+//   $(this).show("active", { direction: "left" }, 1000);
+// });
